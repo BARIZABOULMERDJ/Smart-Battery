32ef2279285ab39a8813a1267b39da8ec9611869
amelieykw <amelieykw1991@gmail.com>
2016-12-26 11:10:08 +0100

    add Google Maps API to this app, enable to show maps

--- app/src/main/java/com/yukaiwen/smartbattery/activities/LoginActivity.java
+++ app/src/main/java/com/yukaiwen/smartbattery/activities/LoginActivity.java
@@ -36,8 +36,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.yukaiwen.smartbattery.R;
 
+import com.yukaiwen.smartbattery.R;
 import java.util.ArrayList;
 import java.util.List;
 
